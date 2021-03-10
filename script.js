3c9af736cf27ad040da8daaab1bd7ce41e668dd5
Matheus Carretta <matheuscarretta@gmail.com>
2021-03-10 16:43:12 -0300

    Ao clicar em um item da lista, adiciona uma cor de fundo

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function createListItem () {
-  let newLi = document.createElement('li');
-  newLi.innerText = inputText.value;
-  document.getElementById('lista-tarefas').appendChild(newLi);
-  inputText.value = ''; 
+  if(inputText.value === ''){
+    alert("Campo de texto vazio, favor preencher");
+  } else{
+    let newLi = document.createElement('li');
+    newLi.innerText = inputText.value;
+    document.getElementById('lista-tarefas').appendChild(newLi);
+    inputText.value = ''; 
+  }
 }
 
 let buttonCreateTask = document.getElementById('criar-tarefa');
 let inputText = document.getElementById('texto-tarefa');
 
 buttonCreateTask.addEventListener('click', createListItem);
+
+let toDoList = document.getElementById('lista-tarefas');
+
+toDoList.addEventListener('click', function(event){
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+})
